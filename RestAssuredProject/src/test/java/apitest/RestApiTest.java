5e0c79bf961257a1c85ac7f73a91f44a050e8f37
unknown <sourav.hit2010@gmail.com>
2021-12-13 17:41:25 +0530

    commit first project

---
+++ RestAssuredProject/src/test/java/apitest/RestApiTest.java
@@ -0,0 +1,26 @@
+package apitest;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class RestApiTest {
+	
+	@Test
+	public void test01()
+	{
+		Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+		
+		if(response.getStatusCode()==200)
+		{
+			System.out.println("Service is returning 200 Success response");
+		}
+		else
+		{
+			System.out.println("Service is not returning 200 Success response");
+		}
+		
+	}
+
+}
